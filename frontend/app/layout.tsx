2516cbe58304f5dca55035f71aee6e56038f6d3b
Full Stack Web Developer <d.hengel@yahoo.com>
2026-08-13 22:04:42 -0400

    Load open editorial workspace styling

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,6 +4,7 @@ import "./brand-logo.css";
 import "./premium-home.css";
 import "./homepage-background-fix.css";
 import "./modern-2026.css";
+import "./open-editorial-workspace.css";
 import { AppShell } from "@/components/AppShell";
 import { LegacyPersonalDefaultsGuard } from "@/components/LegacyPersonalDefaultsGuard";
 
